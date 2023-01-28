72e7a25cb66b652764ece07a69ab9236d5df5fae
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-28 12:49:14 +0600

    first problem solve

---
+++ practice-problem.js
@@ -0,0 +1,10 @@
+// You are given an array:
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+// a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’;
+
+console.log(fruits.indexOf("Banana"));
+fruits[1] = "Mango"
+console.log(fruits)
+
+// banans indexOf 1 and replace ‘Banana’ with ‘Mango’
