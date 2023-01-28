b2ac368a24273a9150a9557f3f9bb943d9720e65
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-28 13:09:12 +0600

    Write a program to find your and your friends’ grades using if-else

--- practice-problem.js
+++ practice-problem.js
@@ -8,3 +8,93 @@ fruits[1] = "Mango"
 console.log(fruits)
 
 // banans indexOf 1 and replace ‘Banana’ with ‘Mango’
+/*
+You and your friends Tom, Jane, Peter and John got their final exam results. Your total
+score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+and John’s total score is 40. The grading chart is 
+ */
+
+var myScore = 85;
+var TomScore = 66;
+var janeScore = 95;
+var peterScore = 56;
+var jhonScore = 40;
+
+if (myScore >= 80){
+    console.log("your garde is A🤗");
+}
+else if(myScore >= 60){
+    console.log("your garde is B");
+
+}
+else if(myScore >= 50){
+    console.log("your garde is C");
+
+}
+else if(myScore >= 40){
+    console.log("your garde is D");
+
+}
+else{
+    console.log("You Grade F. Try next year😢")
+}
+
+// tom
+if (TomScore >= 80){
+    console.log("tom garde is A🤗");
+}
+
+else if(TomScore >= 60){
+    console.log("tom's garde is B");
+
+}
+else if(TomScore >= 50){
+    console.log("tom's garde is C");
+
+}
+else if(TomScore >= 40){
+    console.log("tom's garde is D");
+
+}
+else{
+    console.log("tom's Grade F. Try next year😢")
+}
+// janeScore
+if (janeScore >= 80){
+    console.log("jane garde is A🤗");
+}
+else if(janeScore >= 60){
+    console.log("jane garde is B");
+
+}
+else if(janeScore >= 50){
+    console.log("jane garde is C");
+
+}
+else if(janeScore >= 40){
+    console.log("jane garde is D");
+
+}
+else{
+    console.log("jane Grade F. Try next year😢")
+}
+
+// piter
+if (peterScore >= 80){
+    console.log("peterScore garde is A🤗");
+}
+else if(peterScore >= 60){
+    console.log("v garde is B");
+
+}
+else if(peterScore >= 50){
+    console.log("peterScore garde is C");
+
+}
+else if(peterScore >= 40){
+    console.log("peterScore garde is D");
+
+}
+else{
+    console.log("jane Grade F. Try next year😢")
+}
